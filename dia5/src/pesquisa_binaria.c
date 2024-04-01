4ea3da76793273da4f930b2f792244b2c058120a
Mateus de Souza <mathrews>
2024-04-01 13:16:52 -0300

    aprendendo pelo W3C

--- dia5/src/pesquisa_binaria.c
+++ dia5/src/pesquisa_binaria.c
@@ -3,18 +3,17 @@
 #include <stdio.h>
 #include <strings.h>
 
-int pesquisa_binaria(int *lista, int item)
-{
+int pesquisa_binaria(int *lista, int item) {
   double baixo = 0.0;
   size_t alto = sizeof(lista) - 1;
-  
+
   while (baixo <= alto) {
     int meio = round((baixo + alto) / 2);
     int chute = lista[meio];
-    
+
     if (chute == item) {
       return meio;
-    } 
+    }
     if (chute > item) {
       alto = meio - 1;
     } else {
