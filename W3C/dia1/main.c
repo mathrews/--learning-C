8718ca0758dc37ccbb8b706c05013edb23baad64
Mateus de Souza <mathrews>
2024-04-03 10:05:29 -0300

    Parte do aprendizado feito

--- W3C/dia1/main.c
+++ W3C/dia1/main.c
@@ -2,5 +2,14 @@
 
 int main (void) {
   printf("Hello world!\n");
+  int myNum = 15;
+  printf("A idade que eu tinha ano passado era %d\n", myNum);
+  char myName[7];
+  scanf("%6s", myName);
+  printf("My name is %s\n", myName);
+  char myLetter = 'A';
+  printf("My last name is %c", myLetter);
+  float myFloatNum = 5.99;
+  printf("%f\n", myFloatNum);
   return 0;
 }
