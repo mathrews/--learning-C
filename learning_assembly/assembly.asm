2716bfdfd1a22c89ed217e1a392e0697ad0bad36
mathrews <mateusps4gg@gmail.com>
2024-11-20 12:03:15 -0300

    learning more about registers and memory address

--- learning_assembly/assembly.asm
+++ learning_assembly/assembly.asm
@@ -4,5 +4,8 @@ section .text
 
 global assembly
 assembly:
-  mov eax, 888
+  mov eax, 500 ; Registrando o numero 888 no registrador eax
+  mov ebx, 500 ; Registrando o numero 777 no registrador ebx
+  add eax, ebx ; Adicionando o valor do registrador ebx ao registrador eax
+  mov [0x100], eax ; Registrando o valor do registrador eax na memória
   ret
